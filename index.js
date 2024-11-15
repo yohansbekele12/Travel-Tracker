dd63bcb309d44f5f36eafc0965fbae3c7b215661
yohansbekele12 <yohansdemisie@gmail.com>
2024-11-15 16:44:33 +0100

    error handling

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const db = new pg.Client({
 
 db.connect();
 let country_code = [];
-let total = 0;
 
 const app = express();
 const port = 3000;
@@ -24,61 +23,76 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Middleware for checking visited countrys
+
+async function getVisitedCountries() {
+  const result = await db.query("SELECT country_code FROM visited_countries ");
+
+  return result.rows.map((rows) => rows.country_code);
+}
+
 app.get("/", async (req, res) => {
-  country_code = [];
-  const result = await db.query(
-    "SELECT country_code FROM public.visited_countries"
-  );
+  try {
+    const country_code = await getVisitedCountries();
+    const total = country_code.length;
 
-  const data = result.rows;
-
-  data.forEach((country) => {
-    country_code.push(country.country_code);
-  });
-
-  console.log(country_code);
-  total = data.length;
-
-  // rendern the result
-  res.render("index", { countries: country_code, total });
+    res.render("index", { countries: country_code, total });
+  } catch (error) {
+    console.error("Error while querying the database: ", error);
+    res.status(500).send("Server Error");
+  }
 });
 
 //post route qurey data from countries Db using user inpute
 let test = "";
 app.post("/add", async (req, res) => {
-  const input = req.body["country"].toLowerCase(); //access property of inpute
+  const input = req.body["country"].toLowerCase();
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE country_name LIKE '%' || $1 || '%'",
+      [input]
+    );
+    const data = result.rows;
+    if (data.length !== 0) {
+      data.forEach((country) => {
+        country_code.push(country.country_code);
+        test = country.country_code;
+      });
 
-  // query countries DB for country code
-  const result = await db.query(
-    "SELECT country_code FROM countries WHERE country_name =$1",
-    [input]
-  );
-  const data = result.rows;
-  if (data.length !== 0) {
-    data.forEach((country) => {
-      country_code.push(country.country_code);
-      test = country.country_code;
-    });
+      // Store the country code in visited countries DB             // Avoid duplicates ON CONFLICT (country_code) DO NOTHING
+      try {
+        const result = await db.query(
+          "INSERT INTO visited_countries (country_code) VALUES ($1) ON CONFLICT (country_code) DO NOTHING ",
+          [test]
+        );
 
-    // Store the counries code in visited countries DB             // Avoid duplicates ON CONFLICT (country_code) DO NOTHING
-    try {
-      const result = await db.query(
-        "INSERT INTO visited_countries (country_code) VALUES ($1) ON CONFLICT (country_code) DO NOTHING ",
-        [test]
-      );
-
-      if (result.rowCount > 0) {
-        console.log("insert successfull ");
-      } else {
-        console.log("data alredy exist or insertion faild ");
+        if (result.rowCount > 0) {
+          console.log("insert successfull ");
+          res.redirect("/");
+        } else {
+          const countries = await getVisitedCountries();
+          res.render("index", {
+            countries: countries,
+            error: "data alredy exist please inset new Country",
+            total: countries.length,
+          });
+          console.log("data alredy exist or insertion faild ");
+        }
+      } catch (error) {
+        console.error("faild query requeste  ");
       }
-    } catch (error) {
-      console.error("faild query requeste  ");
+    } else {
+      const countries = await getVisitedCountries();
+      res.render("index", {
+        countries: countries,
+        error: "country not found",
+        total: countries.length,
+      });
+      console.log("country not found ");
     }
-
-    res.redirect("/");
-  } else {
-    console.error("data return undifined or not requested correctly ");
+  } catch (error) {
+    console.error("Error while querying the database: ", error);
+    res.status(500).send("Server Error");
   }
 });
 
